76204e6cf4507f01b81c9a3866c7f05b67abb999
Mohana LK Lingabathina <mohanlingabathina8@gmail.com>
2026-09-26 11:17:25 +0530

    Fix portfolio theme import for Render build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import '../public/portfolio-theme.css';
+import './portfolio-theme.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
